687c4284479c19c2446015ba54577a4f4883d9c8
Your Full Name <youremail@example.com>
2025-05-30 17:16:20 +0300

    Update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import {
   FaHome,
-  FaUser,
   FaShoppingCart,
   FaEnvelope,
   FaCogs,
@@ -16,11 +15,6 @@ const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
         <li>
           <Link to="/home" className="flex items-center gap-2 p-2 hover:bg-gray-700 rounded">
             <FaHome /> Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/profile" className="flex items-center gap-2 p-2 hover:bg-gray-700 rounded">
-            <FaUser /> Profile
           </Link>
         </li>
         <li>
