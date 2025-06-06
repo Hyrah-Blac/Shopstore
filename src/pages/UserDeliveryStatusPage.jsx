c2aad1a811c408f1250703a291a6b5e2df655ee9
Your Full Name <youremail@example.com>
2025-06-06 19:58:06 +0300

    Update

--- src/pages/UserDeliveryStatusPage.jsx
+++ src/pages/UserDeliveryStatusPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './UserDeliveryStatusPage.css';
+import './UserDeliveryStatus.css';
 
 const UserDeliveryStatus = () => {
   const [orders, setOrders] = useState([]);
