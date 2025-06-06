8b66219a0faaf9e81def1eaf0fb07714c63be6b4
Your Full Name <youremail@example.com>
2025-06-06 19:42:51 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import NotFound from "./pages/NotFound";
 import EditProductPrices from "./pages/EditProductPrices";
 import Contacts from "./pages/Contacts";
 import Profile from "./pages/Profile";
-import AdminOrdersPage from "./pages/AdminOrdersPage"; // make sure path is correct
+import AdminOrdersPage from "./pages/AdminOrders"; // make sure path is correct
 import UserDeliveryStatusPage from "./pages/UserDeliveryStatusPage";
 
 // Styles
